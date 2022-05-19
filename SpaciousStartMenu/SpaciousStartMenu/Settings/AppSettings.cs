caf3017da372591507e441b0ac51413d9a3a35a9
3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg <99333667+3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg@users.noreply.github.com>
2022-05-19 21:32:58 +0900

    v1.0.1.0

--- SpaciousStartMenu/SpaciousStartMenu/Settings/AppSettings.cs
+++ SpaciousStartMenu/SpaciousStartMenu/Settings/AppSettings.cs
@@ -3,9 +3,9 @@
     public class AppSettings
     {
         public double Scale { get; set; } = 1.0;
-        public bool RegStartupShortcut { get; set; } = false;
         public bool EscKeyMinimize { get; set; } = true;
         public bool MarginDoubleClickMinimize { get; set; } = false;
         public bool ConfirmCloseMenu { get; set; } = false;
+        public bool MinimizeStartup { get; set; } = false;
     }
 }
