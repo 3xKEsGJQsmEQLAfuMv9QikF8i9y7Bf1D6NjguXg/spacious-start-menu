31d16434dd8723b3d2639f4be508e74404d5f0c4
3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg <99333667+3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg@users.noreply.github.com>
2022-05-21 19:46:54 +0900

    v1.0.3.0

---
+++ SpaciousStartMenu/SpaciousStartMenu/Win32/Window.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SpaciousStartMenu.Win32
+{
+    internal class Window
+    {
+        public const uint SW_SHOWMAXIMIZED = 3;
+
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, uint flags);
+
+        public static void ActivateWindow(IntPtr hWnd)
+        {
+            ShowWindow(hWnd, SW_SHOWMAXIMIZED);
+            SetForegroundWindow(hWnd);
+        }
+
+    }
+}
