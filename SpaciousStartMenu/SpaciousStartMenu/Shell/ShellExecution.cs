7798470f01474b4618292e8b3406f9232083a889
3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg <99333667+3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg@users.noreply.github.com>
2022-06-18 21:26:28 +0900

    v1.2.0.0

--- SpaciousStartMenu/SpaciousStartMenu/Shell/ShellExecution.cs
+++ SpaciousStartMenu/SpaciousStartMenu/Shell/ShellExecution.cs
@@ -1,16 +1,59 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace SpaciousStartMenu.Shell
 {
     internal class ShellExecution
     {
-        public static void Run(string cmd)
+        private static readonly Regex _specialFolder = new("<(.+?)>");
+        private static readonly Regex _envFolder = new("<ENV:(.+?)>");
+
+        public static void Run(string cmd, string? workDir, string? args)
         {
             var p = new Process();
-            p.StartInfo.FileName = cmd;
+            p.StartInfo.FileName = ReplaceSpecialFolder(cmd);
+            if (!string.IsNullOrWhiteSpace(workDir))
+            {
+                p.StartInfo.WorkingDirectory = ReplaceSpecialFolder(workDir);
+            }
+            if (!string.IsNullOrWhiteSpace(args))
+            {
+                p.StartInfo.Arguments = args;
+            }
             p.StartInfo.UseShellExecute = true;
 
             p.Start();
+        }
+
+        private static string ReplaceSpecialFolder(string path)
+        {
+            const string pattern = "<(.+?)>";
+
+            return Regex.Replace(path, pattern, MatchEvaluator);
+        }
+
+        private static string MatchEvaluator(Match m)
+        {
+            const string envMark = "ENV:";
+            if (m.Groups[1].Value.StartsWith(envMark, StringComparison.OrdinalIgnoreCase))
+            {
+                string? env = Environment.GetEnvironmentVariable(
+                    m.Groups[1].Value.Substring(envMark.Length, m.Groups[1].Value.Length - envMark.Length));
+                if (!string.IsNullOrEmpty(env))
+                {
+                    return env;
+                }
+            }
+            else
+            {
+                if (Enum.TryParse(m.Groups[1].Value, out Environment.SpecialFolder sf))
+                {
+                    return Environment.GetFolderPath(sf);
+                }
+            }
+
+            return m.Value;
         }
     }
 }
