26132de4648093b4b712cafdd2ccc81f40761e0b
3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg <99333667+3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg@users.noreply.github.com>
2022-06-18 21:25:17 +0900

    v1.2.0.0

---
+++ SpaciousStartMenu/SpaciousStartMenu/Extensions/WindowExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+
+namespace SpaciousStartMenu.Extensions
+{
+    public static class WindowExtensions
+    {
+        private const double _minSize = 150.0;
+        private const double _maxSizeW = 9000.0;
+        private const double _maxSizeH = 5000.0;
+
+        public static void SetWindowSize(
+            this Window window,
+            double height,
+            double width,
+            bool maximized = false)
+        {
+            if (height < _minSize ||
+                width < _minSize ||
+                _maxSizeH < height ||
+                _maxSizeW < width)
+            {
+                return;
+            }
+
+            window.Height = height;
+            window.Width = width;
+
+            if (maximized)
+            {
+                window.WindowState = WindowState.Maximized;
+            }
+        }
+
+        public static void SetWindowPosition(
+            this Window window,
+            double left,
+            double top)
+        {
+            if (0 <= left &&
+                0 <= top)
+            {
+                window.Left = left;
+                window.Top = top;
+            }
+        }
+    }
+}
