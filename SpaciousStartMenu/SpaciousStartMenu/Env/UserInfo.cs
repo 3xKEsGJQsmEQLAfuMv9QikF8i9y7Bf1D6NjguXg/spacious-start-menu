7ad2fa8ab8368e39e67cc1b47ea10c218624e0ce
3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg <99333667+3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg@users.noreply.github.com>
2022-07-07 20:39:35 +0900

    v1.2.2.0

---
+++ SpaciousStartMenu/SpaciousStartMenu/Env/UserInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.DirectoryServices;
+using System.Threading.Tasks;
+
+namespace SpaciousStartMenu.Env
+{
+    internal class UserInfo
+    {
+        private string? _displayName;
+        private readonly string _initialDisplayName;
+
+        public UserInfo(string initialDisplayName)
+        {
+            _initialDisplayName = initialDisplayName;
+        }
+
+        public string GetUserName()
+        {
+            return Environment.UserName;
+        }
+
+        public string GetDisplayName()
+        {
+            if (_displayName is not null)
+            {
+                return _displayName;
+            }
+
+            string path = $"WinNT://{Environment.UserDomainName}/{Environment.UserName}";
+
+            using var de = new DirectoryEntry(path);
+
+            string result = de.Properties["FullName"]?.Value?.ToString() ?? "";
+            if (string.IsNullOrEmpty(result))
+            {
+                return _initialDisplayName;
+            }
+            else
+            {
+                _displayName = result;
+                return _displayName;
+            }
+        }
+    }
+}
