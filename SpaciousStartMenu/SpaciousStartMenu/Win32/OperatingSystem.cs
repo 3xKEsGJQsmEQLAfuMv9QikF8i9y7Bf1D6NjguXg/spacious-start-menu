ae41c63814a133771032f7b0df23b775cd18c677
3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg <99333667+3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg@users.noreply.github.com>
2022-07-07 20:43:32 +0900

    v1.2.2.0

---
+++ SpaciousStartMenu/SpaciousStartMenu/Win32/OperatingSystem.cs
@@ -0,0 +1,14 @@
+﻿using SpaciousStartMenu.Shell;
+
+namespace SpaciousStartMenu.Win32
+{
+    internal static class OperatingSystem
+    {
+        public static void Signout() => RunShutdownCmd("/l");
+        public static void Shutdown() => RunShutdownCmd("/s /t 0");
+        public static void Restart() => RunShutdownCmd("/r /t 0");
+        
+        private static void RunShutdownCmd(string args) =>
+            ShellExecution.RunCommand("shutdown.exe", args);
+    }
+}
