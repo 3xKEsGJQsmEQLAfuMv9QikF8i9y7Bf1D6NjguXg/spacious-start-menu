90a454786e89377e2386d8a69a10c9f5c0cad45d
3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg <99333667+3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg@users.noreply.github.com>
2022-07-07 20:40:45 +0900

    v1.2.2.0

---
+++ SpaciousStartMenu/SpaciousStartMenu/Processings/NumberOfExecution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SpaciousStartMenu.Processings
+{
+    internal class NumberOfExecution
+    {
+        private int _execCnt = 0;
+        private readonly Action _action;
+
+        public NumberOfExecution(Action act) => _action = act;
+
+        public void RunOnlyOnce()
+        {
+            if (0 < _execCnt)
+            {
+                return;
+            }
+
+            _action();
+            _execCnt++;
+        }
+    }
+}
