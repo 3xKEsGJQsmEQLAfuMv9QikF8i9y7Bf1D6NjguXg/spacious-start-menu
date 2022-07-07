a5775603247f85197dec2f8c6726bfee21f1e6ff
3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg <99333667+3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg@users.noreply.github.com>
2022-07-07 20:39:07 +0900

    v1.2.2.0

---
+++ SpaciousStartMenu/SpaciousStartMenu/Converters/PercentageConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SpaciousStartMenu.Converters
+{
+    [ValueConversion(typeof(double), typeof(string))]
+    internal class PercentageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not double d)
+            {
+                return "";
+            }
+
+            return $"{Math.Floor(d * 100)}%";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
