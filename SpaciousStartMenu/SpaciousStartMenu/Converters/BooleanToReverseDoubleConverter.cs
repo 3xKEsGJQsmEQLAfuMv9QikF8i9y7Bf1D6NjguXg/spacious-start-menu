f3d1d6e7f83be0213bf70e5482c2d4b83e4dccfb
3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg <99333667+3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg@users.noreply.github.com>
2022-07-29 20:28:45 +0900

    v1.2.5.0

---
+++ SpaciousStartMenu/SpaciousStartMenu/Converters/BooleanToReverseDoubleConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SpaciousStartMenu.Converters
+{
+    [ValueConversion(typeof(bool), typeof(double))]
+    internal class BooleanToReverseDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not bool b ||
+                parameter is null)
+            {
+                return 1.0;
+            }
+
+            if (double.TryParse(parameter.ToString(), out double param))
+            {
+                return b ? 1.0 : param;
+            }
+            else
+            {
+                return 1.0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+
+    }
+}
---
+++ SpaciousStartMenu/SpaciousStartMenu/Converters/BooleansToBooleanConverter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace SpaciousStartMenu.Converters
+{
+    internal enum TrueFalseJudgeType
+    {
+        OrTrueToTrue,
+        OrTrueToFalse,
+        AndTrueToTrue,
+        AndTrueToFalse,
+    }
+
+    [ValueConversion(typeof(bool), typeof(bool?))]
+    internal class BooleansToBooleanConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values is null ||
+                values.Length < 1 ||
+                parameter is not string p)
+            {
+                return false;
+            }
+
+            var t = GetParameter(p);
+
+            if (t == TrueFalseJudgeType.OrTrueToTrue)
+            {
+                return values.Any(x => x is bool b && b);
+            }
+
+            if (t == TrueFalseJudgeType.OrTrueToFalse)
+            {
+                return !values.Any(x => x is bool b && b);
+            }
+
+            return false;
+        }
+
+        private TrueFalseJudgeType GetParameter(string value)
+        {
+            if (!Enum.TryParse(value, out TrueFalseJudgeType t))
+            {
+                throw new ArgumentException("Parameter Error");
+            }
+
+            switch (t)
+            {
+                case TrueFalseJudgeType.OrTrueToTrue:
+                    break;
+                case TrueFalseJudgeType.OrTrueToFalse:
+                    break;
+                case TrueFalseJudgeType.AndTrueToTrue:
+                    throw new NotImplementedException();
+                case TrueFalseJudgeType.AndTrueToFalse:
+                    throw new NotImplementedException();
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return t;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
